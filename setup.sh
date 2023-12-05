ebba5d342b6b6825b048a3ce609c75943d4c8296
hben35 <99895665+hben35@users.noreply.github.com>
2023-12-05 15:52:43 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install -y vim git
+apt-get update
+apt-get upgrade -y
+apt-get install -y vim git
 
 # Install Python
-sudo apt-get install -y python3 python3-virtualenv python-is-python3 python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+apt-get install -y python3 python3-virtualenv python-is-python3 python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
 
 # Install PyICU dependencies
 # https://community.libretranslate.com/t/pyicu-fails-to-install-on-ubuntu-20-04/23
-sudo apt-get install -y libicu-dev python3-icu pkg-config
+apt-get install -y libicu-dev python3-icu pkg-config
 
 # Install Nginx
-sudo apt-get install -y nginx
+apt-get install -y nginx
 
 # Download LibreTranslate source
 git clone https://github.com/LibreTranslate/LibreTranslate.git ~/LibreTranslate
