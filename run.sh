3a15908b27c9cd629a3aab457dc43b12be2c2ed2
hben35 <99895665+hben35@users.noreply.github.com>
2023-12-05 15:53:10 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # Setup systemd
-sudo cp ~/LibreTranslate-init/libretranslate.service /etc/systemd/system/
-sudo systemctl start libretranslate
-sudo systemctl enable libretranslate
+cp ~/LibreTranslate-init/libretranslate.service /etc/systemd/system/
+systemctl start libretranslate
+systemctl enable libretranslate
 # sudo systemctl status libretranslate
 
 # Configure Nginx
-sudo cp ~/LibreTranslate-init/nginx /etc/nginx/sites-available/default
-sudo nginx -t
-sudo systemctl restart nginx
+cp ~/LibreTranslate-init/nginx /etc/nginx/sites-available/default
+nginx -t
+systemctl restart nginx
 
